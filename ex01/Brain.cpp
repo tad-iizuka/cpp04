da487fd758eb93eb54cb2b330f844f45843aa6a7
tad-iizuka <tad.iizuka@gmail.com>
2025-07-27 20:51:16 +0900

    wip

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -6,13 +6,16 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 17:29:50 by tiizuka           #+#    #+#             */
-/*   Updated: 2025/07/27 19:01:23 by tiizuka          ###   ########.fr       */
+/*   Updated: 2025/07/27 20:47:42 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
 Brain::Brain( void ) {
+	for (int i = 0; i < SIZE; ++i) {
+			ideas[i] = "empty";
+	}
 	std::cout << C_B << "Brain constructed."
 		<< C_CLR << std::endl;
 }
@@ -27,7 +30,8 @@ Brain::Brain( const Brain& src ) {
 Brain& Brain::operator=( const Brain& rhs ) {
 	if (this != &rhs)
 	{
-		operator=(rhs);
+		for (int i = 0; i<SIZE; ++i)
+			ideas[i] = rhs.ideas[i];
 		std::cout << C_B << "Brain assignation operator copied."
 			<< C_CLR << std::endl;
 	}
--- ex01/Cat.cpp
+++ ex01/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 04:07:02 by tiizuka           #+#    #+#             */
-/*   Updated: 2025/07/27 19:53:54 by tiizuka          ###   ########.fr       */
+/*   Updated: 2025/07/27 20:50:45 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,45 +28,18 @@ Cat::Cat( void ) : Animal("Cat") {
 }
 
 Cat::Cat( const Cat& src ) : Animal(src) {
-	Brain *temp;
-
-	try {
-		temp = new Brain();
-	} catch (std::bad_alloc& e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-		temp = NULL;
-	}
-	if (temp != NULL)
-	{
-		for (int i = 0; i<SIZE; ++i)
-			temp[i] = src.brain[i];
-		delete brain;
-		brain = temp;
-	}
 	type = src.getType();
+	brain = new Brain(*src.brain);
 	std::cout << C_B << "Cat " << src.getType() <<  " copied."
 		<< C_CLR << std::endl;
 }
 
 Cat& Cat::operator=( const Cat& rhs ) {
-	Brain *temp;
-
 	if (this != &rhs)
 	{
-		try {
-			temp = new Brain();
-		} catch (std::bad_alloc& e) {
-			std::cerr << "Error: " << e.what() << std::endl;
-			temp = NULL;
-		}
-		if (temp != NULL)
-		{
-			for (int i = 0; i<SIZE; ++i)
-				temp[i] = rhs.brain[i];
-			delete brain;
-			brain = temp;
-		}
 		Animal::operator=(rhs);
+		delete brain;
+		brain = new Brain(*rhs.brain);
 		std::cout << C_B << "Cat " << rhs.getType()
 			<< " assignation operator copied." << C_CLR << std::endl;
 	}
