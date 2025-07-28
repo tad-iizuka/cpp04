61cc0766c6a34ee00c08b6ed78aa6f1f88f58d44
tad-iizuka <tad.iizuka@gmail.com>
2025-07-28 11:12:49 +0900

    update

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 08:01:50 by tiizuka           #+#    #+#             */
-/*   Updated: 2025/07/28 11:02:04 by tiizuka          ###   ########.fr       */
+/*   Updated: 2025/07/28 11:12:41 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ AMateria::AMateria(std::string const & type) {
 }
 std::string const & AMateria::getType() const {
 	return _type;
-} //Returns the materia type
+}
+
 void AMateria::use(ICharacter& target) {
 	(void)target;
 }
